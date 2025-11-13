ce56f67c983e4a986f4f5fdc3c44f96c72079f03
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-11-13 11:21:36 +0900

    feat: LollipopHelper を追加
    Lollipop APIを呼ぶときに使うHelperメソッド集

---
+++ src/Helper/LollipopHelper.ts
@@ -0,0 +1,63 @@
+import type { LollipopResponse } from "../Entity/LollipopResponse.js";
+import { APIError, DataParseError, NetworkError } from "./CustomErrors.js";
+
+export class LollipopHelper {
+	public static readonly instance = new LollipopHelper();
+	private constructor() {}
+
+	/**
+	 *
+	 * @param endpoint
+	 * @param context エラーログ用のコンテキスト情報
+	 * @param options fetchのオプション
+	 * @returns LollipopからのResponseをデコードしたもの
+	 */
+	async fetchAndDecodeLollipopResponse(endpoint: string, context: string, options: RequestInit): Promise<LollipopResponse> {
+		let response: Response;
+
+		// ネットワークリクエストの実行
+		try {
+			response = await fetch(endpoint, options);
+		} catch (error) {
+			throw new NetworkError(`${context} ネットワークエラー: ${error}`);
+		}
+
+		// Lollipopレスポンスのデコード
+		try {
+			return (await response.json()) as LollipopResponse;
+		} catch (error) {
+			throw new DataParseError(`${context} Lollipopレスポンスのデコードに失敗しました。 Response: ${response.body} Error: ${error}`);
+		}
+	}
+
+	/**
+	 * Lollipopレスポンスの検証(response.statusが"success"であることを確認)
+	 * @param response LollipopからのResponse
+	 * @param context エラーログ用のコンテキスト情報
+	 * @param requireData LollipopからのResponseの中にデータの存在を必須とするかどうか
+	 */
+	validateLollipopResponse(response: LollipopResponse, context: string, requireData: boolean = false): void {
+		if (response.status !== "success") {
+			throw new APIError(`${context} APIエラー: ${response.message}`);
+		}
+		if (requireData && !response.data) {
+			throw new DataParseError(`${context} Responseの中にdataが存在しません。Response: ${response}`);
+		}
+	}
+
+	/** Lollipopレスポンスのdataフィールドからデータをデコード */
+	decodeDataFromLollipopResponse<T>(data: string, context: string): T {
+		try {
+			return JSON.parse(data) as T;
+		} catch (error) {
+			throw new DataParseError(`${context} データのデコードに失敗しました。 Error: ${error}`);
+		}
+	}
+
+	// エンドポイントURLの構築
+	buildEndpoint(path: string, params: Record<string, string>): string {
+		const baseURL = import.meta.env.VITE_API_ENDPOINT;
+		const searchParams = new URLSearchParams(params);
+		return `${baseURL}${path}?${searchParams.toString()}`;
+	}
+}
