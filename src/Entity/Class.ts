7ff7a6f9f741920e32ff6ebfbc56b50060e75b50
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-11-14 19:16:37 +0900

    refactor(entity): Classにファクトリ関数を追加

--- src/Entity/Class.ts
+++ src/Entity/Class.ts
@@ -6,6 +6,16 @@ export type Class = {
 	majorCode: string;
 };
 
+export const createClass = (teacherID: string, name: string, admissionYear: string | number, majorCode: string): Class => {
+	return {
+		id: "", // IDは後でAPIから取得するため空文字で初期化
+		teacherID: teacherID,
+		name: name,
+		admissionYear: Number(admissionYear),
+		majorCode: majorCode,
+	};
+};
+
 // APIからの生データの型定義
 export type RawClassResponse = {
 	id: string;
