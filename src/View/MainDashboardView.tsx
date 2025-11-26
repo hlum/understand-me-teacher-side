620f95565dbad76113dc1983836f25b59c392f10
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-11-26 11:53:43 +0900

    ui: ProfileIconを追加

--- src/View/MainDashboardView.tsx
+++ src/View/MainDashboardView.tsx
@@ -6,6 +6,7 @@ import { ThemeToggle } from "./Components/ThemeToggle.js";
 import { useRouteManager } from "../Router/useRouteManager.js";
 import type { User } from "firebase/auth";
 import { UserManager } from "@/Manager/UserManager.js";
+import { ProfileMenu } from "./Components/ProfileMenu.js";
 
 type MainDashboardViewProps = {
 	authData: User;
@@ -26,13 +27,15 @@ const MainDashboardView = ({ authData }: MainDashboardViewProps) => {
 			{/* Header Section */}
 			<div className="flex flex-wrap justify-between items-center mb-10 gap-6">
 				<h1 className="text-3xl sm:text-4xl font-bold text-adaptive">
-					クラス管理、 <span className="text-accent-light">{vm.user?.name}</span>
+					クラス管理、
+					<span className="text-accent-light">{vm.user?.name}</span>
 				</h1>
 
 				<div className="flex items-center gap-4">
 					<ThemeToggle />
+					<ProfileMenu photoURL={vm.user?.photoURL ?? ""} name={vm.user?.name ?? ""} onLogout={() => {}} />
 					<button onClick={() => navigate.toCreateClass()} className="btn-primary">
-						<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+						<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2">
 							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4" />
 						</svg>
 						クラスを追加
